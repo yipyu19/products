0380177d3679c98138fdde467ff2129d4e08dbf6
YIPYU <yipyu19@gmail.com>
2020-10-07 12:17:25 +0800

    check the file address

--- products.py
+++ products.py
@@ -1,11 +1,16 @@
+import os 
 products = []
-with open('products.csv','r', encoding = 'utf-8') as f:
-    for line in f:
-        if 'name, price' in line:
-            continue
-        name, price = line.strip().split(',')
-        products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):
+    print('yeah! found it')
+    with open('products.csv','r', encoding = 'utf-8') as f:
+        for line in f:
+            if 'name, price' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+else:
+    print('can not find the file')
 
 while True:
     name = input('請輸入商品名稱：')
